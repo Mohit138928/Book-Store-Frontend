c16e80087228df5d1a03d8d0b024d83756a61375
Mohit Maurya <mauryamohit138@gmail.com>
2025-03-23 16:04:33 +0530

    Update getBaseUrl function to return different URLs based on the environment

--- src/utils/baseURL.js
+++ src/utils/baseURL.js
@@ -1,5 +1,8 @@
 const getBaseUrl = () => {
-    return "http://localhost:5000"
-}
+  const isDevelopment = import.meta.env.MODE === "development";
+  return isDevelopment
+    ? "http://localhost:5000"
+    : "https://book-store-backend-mocha-one.vercel.app";
+};
 
 export default getBaseUrl;
