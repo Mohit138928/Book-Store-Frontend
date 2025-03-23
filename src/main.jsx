c6fe78a049d40b4c6c680cdc4ddf3b52aa381e94
Mohit Maurya <mauryamohit138@gmail.com>
2025-03-23 15:37:58 +0530

    Refactor main.jsx for improved readability and update import path to lowercase

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import { RouterProvider } from 'react-router-dom'
-import router from './routers/router.jsx'
-import { Provider } from 'react-redux'
-import { store } from './redux/Store.js'
-import 'sweetalert2/dist/sweetalert2.js'
-import 'semantic-ui-css/semantic.min.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { RouterProvider } from "react-router-dom";
+import router from "./routers/router.jsx";
+import { Provider } from "react-redux";
+import { store } from "./redux/store.js"; // Updated import path to lowercase
+import "sweetalert2/dist/sweetalert2.js";
+import "semantic-ui-css/semantic.min.css";
 
-createRoot(document.getElementById('root')).render(
-  <Provider store={store}>
-    <RouterProvider router={router} />
-  </Provider>,
-)
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
+  </StrictMode>
+);
